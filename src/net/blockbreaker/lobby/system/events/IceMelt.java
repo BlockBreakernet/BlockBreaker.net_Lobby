acbd0717c3d397128cae49532c94b2f9a73d28a4
lusu007 <lukas.jost@outlook.com>
2015-04-13 16:21:53 +0200

    Added first JumpPlayte BETA

--- src/net/blockbreaker/lobby/system/events/IceMelt.java
+++ src/net/blockbreaker/lobby/system/events/IceMelt.java
@@ -3,6 +3,7 @@ package net.blockbreaker.lobby.system.events;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockFadeEvent;
+import org.bukkit.event.block.BlockFormEvent;
 
 /**
  * Created by Lukas on 12.04.2015.
@@ -11,6 +12,11 @@ public class IceMelt implements Listener {
 
     @EventHandler
     public void onMelt(BlockFadeEvent e) {
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onForm(BlockFormEvent e) {
         e.setCancelled(true);
     }
 }
---
+++ src/net/blockbreaker/lobby/system/features/JumpPlates.java
@@ -0,0 +1,33 @@
+package net.blockbreaker.lobby.system.features;
+
+import de.slikey.effectlib.util.ParticleEffect;
+import org.bukkit.Material;
+import org.bukkit.Sound;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.util.Vector;
+
+/**
+ * Created by Lukas on 13.04.2015.
+ */
+public class JumpPlates implements Listener {
+
+    @EventHandler
+    public void onMove(PlayerMoveEvent e) {
+        Player p = e.getPlayer();
+
+        if(p.getLocation().getBlock().getType() == Material.GOLD_PLATE) {
+            if(p.getLocation().subtract(0.0D, 1.0D, 0.0D).getBlock().getType() == Material.COBBLESTONE) {
+
+                Vector v = p.getLocation().getDirection().multiply(2.0D).setY(1.0D);
+                p.setVelocity(v);
+
+                ParticleEffect.PORTAL.display(p.getLocation(), 1, 1, 1, 1, 1, 50);
+
+                p.playSound(p.getLocation(), Sound.ENDERDRAGON_WINGS, 1.0F, 1.0F);
+            }
+        }
+    }
+}
